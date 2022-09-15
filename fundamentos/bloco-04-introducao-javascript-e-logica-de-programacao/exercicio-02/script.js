1b09c57672e7a10d00125b0b86455660f7de96aa
viviannemelo <viviannemmelo@gmail.com>
2022-09-15 17:11:06 -0300

    Exercício 02 - Array e For

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/exercicio-02/script.js
@@ -0,0 +1,42 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     // console.log(numbers[i]);
+//     result += numbers[i];
+//     // console.log(result);
+// }
+
+
+// let media = result / numbers.length;
+//     // console.log(media);
+// if (media > 20) {
+//     // console.log('Número maior que 20.');
+// } else if (media < 20) {
+//     // console.log('Número menor que 20');
+// }
+
+
+// let maiorNumero = numbers[0]; //Começo sempre perguntando pro primeiro elemento do array
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > maiorNumero) {
+//         maiorNumero = numbers[index];
+//     }
+// }
+// console.log(maiorNumero);
+
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 !== 0) {
+//     result += 1;
+//   }
+// }
+
+// if (result === 0) {
+//   console.log('nenhum valor ímpar encontrado');
+// } else {
+//   console.log(result);
+// }
+
+
+// FALTA O 7, 8 E 9
