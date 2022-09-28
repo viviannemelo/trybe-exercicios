6df3c64e604575c51027e1104fcf8df013d35bbf
viviannemelo <viviannemmelo@gmail.com>
2022-09-28 16:59:18 -0300

    Exercícios de HOFs dia 1 e dia 2.

---
+++ fundamentos/bloco-08-higher-order-functions/dia-01/script.js
@@ -0,0 +1,33 @@
+//Nova pessoa contratada
+// const dadosFuncionario = (nomeCompleto) => {
+//     const email = nomeCompleto.toLowerCase().split(' ').join('_');
+//     return `${nomeCompleto}, ${email}@trybe.com`;
+//     }
+
+// const newEmployees = (callback) => {
+//     const employees = {
+//       id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//       id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//       id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//     }
+//     return employees;
+//   };
+
+// console.log(newEmployees(dadosFuncionario));
+//pedro_guerra@trybe.com
+
+//Sorteador de loteria
+// const resultadoSorteio = (aposta) => {
+//     if (aposta) {
+//       return `Parabéns você ganhou`  
+//     } else {
+//       return `Tente novamente`
+//     }   
+// }
+
+// const aposta = (array) => {
+//     if (let index = 1; index <= 5; index += 1) {
+
+//     }
+
+// }
